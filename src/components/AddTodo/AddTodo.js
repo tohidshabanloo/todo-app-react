29f023213df4e2b5d5b3afc088e2f3f5f6a309f2
Tohid Shabanloo <tshabanloo@Hotmail.com>
2022-03-26 12:25:26 +0430

    1:25

--- src/components/AddTodo/AddTodo.js
+++ src/components/AddTodo/AddTodo.js
@@ -1,11 +1,23 @@
 import { useState } from "react";
 import "./AddTodo.scss";
+import { v4 as uuid } from "uuid";
 
 const AddTodo = () => {
   const [inputValue, setInputValue] = useState("");
+  const [todoList, setTodoList] = useState([]);
+
+  const addTodo = () => {
+    const newTodo = { id: uuid(), text: inputValue };
+    const updatedTodoList = [...todoList, newTodo];
+    setTodoList(updatedTodoList);
+    setInputValue("");
+  };
+
+  console.log(todoList);
+
   return (
     <div className="add-todo">
-      <button> + </button>
+      <button onClick={addTodo}> + </button>
       <input
         value={inputValue}
         onChange={(e) => setInputValue(e.target.value)}
